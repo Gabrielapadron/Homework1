1d229a1c7843f4f297ede85b54b374ef33cbdcf7
Gabriela Padron <gpadron@cs.csubak.edu>
2015-04-22 14:23:46 -0700

    txt.17

--- lab1.cpp
+++ lab1.cpp
@@ -397,11 +397,11 @@ void render(Game *game)
         ggprint16(&coding, 200*5, cref, "Coding");
         ggprint16(&testing, 300*5, cref, "Testing");
         ggprint16(&maintenance, 400*5, cref, "Maintenance");*/
-        ggprint16(&requirements, 500 - 0, cref, "Requirments");
+        ggprint16(&requirements, 500 * 2, cref, "Requirments");
         ggprint16(&design, 500 - 80, cref, "Design");
         ggprint16(&coding, 500 - 2*80, cref, "Coding");
         ggprint16(&testing, 500 - 3*80, cref, "Testing");
-        ggprint16(&maintenance, 500 - 4*80, cref, "Maintenance");
+        ggprint16(&maintenance, 1000, cref, "Maintenance");
 }
 
 
